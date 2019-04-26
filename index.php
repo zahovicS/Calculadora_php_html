3545a8774ef34522b3f8ad27812832957940e867
zahovicS <zv.elrevelde@gmail.com>
2019-04-25 23:10:52 -0500

    creacion de switch

--- index.php
+++ index.php
@@ -1,9 +1,47 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no">
 	<title>Calculadora</title>
 </head>
 <body>
-
+<form action="Calculadora.php" method="post">
+	<input type="text" name="txtn1">
+	<select name="opciones">
+		<option value="0">Sumar</option>
+		<option value="1">Restar</option>
+		<option value="2">Multiplicar</option>
+		<option value="3">Dividir</option>
+	</select>
+	<input type="text" name="txtn2">
+	<input type="submit" name="calcular" value="calcular">
+</form>
 </body>
 </html>
+
+<?php 
+	if (isset($_REQUEST['calcular'])) {
+		$n1=$_REQUEST['txtn1'];
+		$n2=$_REQUEST['txtn2'];
+		$op=$_REQUEST['opciones'];
+		
+		switch ($op) {
+			case 0:
+				
+				break;
+			case 1:
+				
+				break;
+			case 2:
+				
+				break;
+			case 3:
+				
+				break;
+			default:
+				# code...
+				break;
+		}
+	}
+?>
